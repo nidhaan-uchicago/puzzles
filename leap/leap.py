a847c832c86bc1e48d3ff536c199e3336e37c7c7
Jiaming Wang <jiamingwang@live.com>
2019-09-18 15:19:30 +0000

    Leap function update

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,16 @@
 def leap_year(year):
-    pass
+    '''
+    on every year that is evenly divisible by 4
+      except every year that is evenly divisible by 100
+    unless the year is also evenly divisible by 400
+    '''
+    
+    if year % 4 != 0:
+        print("{} is not a leap year".format(year))
+    else: 
+        if year % 400 == 0:
+            print("{} is a leap year".format(year))
+        elif year % 100 == 0:
+            print("{} is not a leap year".format(year))
+        else:
+            print("{} is a leap year".format(year))
