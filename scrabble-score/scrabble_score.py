6d1e6f4a296fef5d91e442bc1abbcb3c185f2322
Nidhaanjit Jain <nidhaanjit.jain@chicagobooth.edu>
2019-09-18 19:34:34 +0000

    Incorrect code for the scrabble task

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,25 @@
 def score(word):
-    pass
+    letter_list= list(word)
+    score= 0
+    for letter in letter_list: 
+        if letter== "a" or letter== "e" or letter== "i" or letter== "o" or letter== "u" or letter== "l" or letter== "n" or letter== "r" or letter== "s" or letter== "t":
+            score= score + 1
+        elif letter== "d" or letter== "g":
+            score == score + 2
+        elif letter== "b" or letter== "c" or letter== "p" or letter== "m":
+            score = score + 3
+        elif letter== "f" or letter== "h" or letter== "v" or letter== "w" or letter== "y":
+            score= score + 4
+        elif letter== "k":
+            score= score + 5
+        elif letter== "j" or letter== "x":
+            score= score + 8
+        else:
+            score= score + 10
+            
+    print(score)
+            
+        
+        
+
+
