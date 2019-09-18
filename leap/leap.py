5763ba524b922146ff16d051c02058a6e0d3f113
Emma Zhang <emilyluzhang@gmail.com>
2019-09-18 15:40:12 +0000

    test

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,9 @@
 def leap_year(year):
-    pass
+    if (year % 4) != 0:
+        print("No")
+    else:
+       if (year % 100) == 0 & (year % 400) != 0:
+            print("No")
+       elif (year % 100) == 0 & (year % 400) == 0: 
+            print("Yes")
+        
