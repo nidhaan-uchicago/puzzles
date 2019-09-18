dd048c755644bfd86527098206c453bce990a720
Emma Zhang <emilyluzhang@gmail.com>
2019-09-18 18:26:36 +0000

    We did it maybe

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,15 @@
 def steps(number):
-    pass
+    i = 0
+    while (number != 1):
+        if (number % 2) == 0:
+            # divide number by 2, store as new number
+            number = number/2
+            i = i + 1
+        else:
+            # do 3x + 1, store as new number
+            number = 3*number + 1
+            i = i + 1
+            
+    print(i)
+    
+    
